d30a1b401110236861f40cd9715b2095fbaf974d
Jsnn <jsnnmsc0421@gmail.com>
2024-05-12 19:10:19 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyRGBPanel from './components/MyRGBPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>4B0G0093 5/9作業</h1>
+        <MyRGBPanel/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
