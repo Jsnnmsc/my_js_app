1b732fa9615063f414d02dea90c92fd7597c8fee
Jsnn <jsnnmsc0421@gmail.com>
2024-05-16 21:24:27 +0800

    5/16 homework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import MyRGBPanel from './components/MyRGBPanel';
+import MyCalculator from './components/MyCalculator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>4B0G0093 5/9作業</h1>
-        <MyRGBPanel/>
+        <h1>4B0G0093 5/16作業</h1>
+        <h2>MyRGBPanel</h2>
+        <MyRGBPanel />
+        <h2>MyCalculator</h2>
+        <MyCalculator />
       </header>
     </div>
   );
